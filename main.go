7a31484435aea45952f4c43e41a1a2b45fbc2d23
iqqu <33103874+iqqu@users.noreply.github.com>
2025-09-18 17:19:20 -0400

    Only select thumbnail from fetched files

--- main.go
+++ main.go
@@ -534,7 +534,7 @@ func handleBrowse(w http.ResponseWriter, r *http.Request) {
 					     , mt.name AS mime_type
 					  FROM remote_file rf
 					  LEFT JOIN mime_type mt ON mt.mime_type_id = rf.mime_type_id
-					 WHERE rf.remote_file_id = ?
+					 WHERE rf.remote_file_id = ? AND rf.fetched = 1
 			`, thumb.FileId).Scan(&thumb.Filename, &thumb.MimeType)
 			}); err != nil {
 				return err
